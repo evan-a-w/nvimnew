d6a09805a693dae7dd2c9835c2a826b6e6ec9250
Evan Williams <evanawilliams@live.com>
2022-12-28 19:53:53 +0800

    😊

--- lua/completion.lua
+++ lua/completion.lua
@@ -5,17 +5,17 @@ local cmp = require "cmp"
 
 cmp.setup {
     mapping = {
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
 
-        ["<C- >"] = cmp.mapping.complete(),
+        ["<Tab>"] = cmp.mapping.complete(),
 
         ["<C-c>"] = cmp.mapping.close(),
-        ["<C-y>"] = cmp.mapping.confirm {
+        ["<cr>"] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Insert,
             select = true,
         },
-        ["<C-d>"] = function(fallback)
+        ["<C-j>"] = function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
             else
@@ -23,7 +23,7 @@ cmp.setup {
             end
         end,
 
-        ["<C-u>"] = function(fallback)
+        ["<C-k>"] = function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
             else
