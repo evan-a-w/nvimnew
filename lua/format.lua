026643f2ed6d4c412621cda3009b1e62250e86d1
Evan Williams <evanawilliams@live.com>
2023-01-08 17:58:38 +0800

    😊

--- lua/format.lua
+++ lua/format.lua
@@ -10,6 +10,14 @@ vim.cmd([[
                 \ }
     let g:neoformat_enabled_ocaml = ['ocamlformat']
 
+    " custom setting for clangformat
+    let g:neoformat_cpp_clangformat = {
+        \ 'exe': 'clang-format',
+        \ 'args': ['--style="{IndentWidth: 4}"']
+    \}
+    let g:neoformat_enabled_cpp = ['clangformat']
+    let g:neoformat_enabled_c = ['clangformat']
+
     augroup fmt
       autocmd!
       autocmd BufWritePre * | Neoformat
